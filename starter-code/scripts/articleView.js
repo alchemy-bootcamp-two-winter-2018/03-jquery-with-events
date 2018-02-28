bc4989b2078a4eb49d4da6c0b4b17ca14b9c79d9
Ryan Manro <rmanro@gmail.com>
2018-02-28 15:30:47 -0800

    pushing to review

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -71,7 +71,7 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function() {
-    // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+    // TODOne: Add an event handler to .main-nav elements that will power the Tabs feature.
     // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
     // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
 
@@ -95,6 +95,12 @@ articleView.setTeasers = function() {
 
     // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
     // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+    $('article .read-on').on('click', function () {
+        $(this).hide();
+        const $previous = $(this).prev();
+        console.log($previous);
+        $(`${$previous} p`).show();
+    }); 
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
@@ -106,3 +112,4 @@ articleView.populateFilters();
 articleView.handleAuthorFilter();
 articleView.handleCategoryFilter();
 articleView.handleMainNav();
+articleView.setTeasers();
