1511feda8139d1b0a2b00cfed0958bb289a81b1c
Keli Hansen <keli.p.hansen@gmail.com>
2018-02-28 15:37:13 -0800

    complete setTeasers method

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -77,7 +77,8 @@ articleView.handleMainNav = function() {
     // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
     // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
 
-    $('.main-nav .tab').on('click', function () {
+    $('.main-nav .tab').on('click', function (event) {
+        event.preventDefault();
         $('.tab-content').hide();
         const dataContent = $(this).attr('data-content');
         $(`section[id="${dataContent}"]`).show();
@@ -93,6 +94,12 @@ articleView.setTeasers = function() {
 
     // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
     // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
+    $('#articles').on('click', '.read-on', function(event) {
+        event.preventDefault();
+        const thisArticle = $(this).parent();
+        thisArticle.find('.article-body *:nth-of-type(n+2)').show();
+        $(this).hide();
+    });
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
@@ -101,5 +108,5 @@ $(document).ready(function() {
     articleView.handleAuthorFilter();
     articleView.handleCategoryFilter();
     articleView.handleMainNav();
-    // articleView.setTeasers();
+    articleView.setTeasers();
 });
