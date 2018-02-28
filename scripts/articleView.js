d7063cd95677afca3d4ab19ed4d0aa016ccfc1db
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-02-28 14:37:05 -0800

    hide articles and faded in selected articles

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -42,6 +42,10 @@ articleView.handleAuthorFilter = function() {
         if ($(this).val()) {
             // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
             // Use an "attribute selector" to find those articles, and fade them in for the reader.
+            const authorName = $(this).val();
+            $('article').hide();
+            $(`article[data-js-author="${authorName}"]`).fadeIn();
+            // $('article').attr('data-js-author', authorName);
 
         } else {
             // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles.
@@ -76,5 +80,9 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+    articleView.populateFilters();
+    articleView.handleAuthorFilter();
+    // articleView.handleCategoryFilter();
+    // articleView.handleMainNav();
+    // articleView.setTeasers();
 });
